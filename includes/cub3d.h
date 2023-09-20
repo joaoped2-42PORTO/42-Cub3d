90f4c68a4e30c724af05157d010f731e8d6c18a0
joaoped2 <joaoped2@c1r12s17.42porto.com>
2023-09-20 13:13:25 +0100

    minor changes on the header file and added struct for player

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,25 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huolivei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 14:22:56 by huolivei          #+#    #+#             */
-/*   Updated: 2023/09/18 15:36:57 by huolivei         ###   ########.fr       */
+/*   Updated: 2023/09/20 13:12:34 by joaoped2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../libft/libft.h"
+# include <fcntl.h>
 # include <math.h>
-# include <stdlib.h>
-# include <unistd.h>
 # include <stdbool.h>
 # include <stdio.h>
-# include <fcntl.h>
-# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
 
-typedef	struct	t_game {
+typedef struct s_game
+{
 	char	**map;
 	int		fd;
 	int		max_x;
@@ -32,17 +33,21 @@ typedef	struct	t_game {
 	char	*w_texture;
 	char	*floor_texture;
 	char	*ceeling_texture;
-}	s_game;
+}			t_game;
 
+typedef struct s_player
+{
+	float	player_y;
+	float	player_x;
+}			t_player;
 
-/* 			mapCheck.c			 */
-void	read_map(s_game *game, char **av);
-int		size_map(s_game *game);
-bool	valid_map(s_game *game);
+/* 			mapCheck.c				*/
+void		read_map(s_game *game, char **av);
+int			size_map(s_game *game);
+bool		valid_map(s_game *game);
 
-/* 			cleaner.c			 */
-void	free_game(s_game *game);
-void	free_matrix(char **str);
-
+/* 			cleaner.c				*/
+void		free_game(s_game *game);
+void		free_matrix(char **str);
 
 #endif
