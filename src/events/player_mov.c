e8660707b1dfd732ce542a31fa829913e48693d7
root <root@DESKTOP-TK6IIHV>
2023-10-03 15:25:00 +0100

    testing walls

--- src/events/player_mov.c
+++ src/events/player_mov.c
@@ -30,6 +30,7 @@ void move_down(t_game *game)
 {
 	char test;
 
+	printf("1st: %f\n", game->player.posY);
 	test = game->map[(int)(game->player.posY + game->player.planeX * 0.1f)][(int)(game->player.posX)];
 	if (test == '0' || test == 'O' || test == ' ' || test == 'N' || test == 'S' || test == 'E' || test == 'W')
 		game->player.posY += game->player.planeX * game->player.m_speed;
@@ -46,10 +47,33 @@ void move_up(t_game *game)
 
 	test = game->map[(int)(game->player.posY + game->player.planeX * 0.1f)][(int)(game->player.posX)];
 	if (test == '0' || test == 'O' || test == ' ' || test == 'N' || test == 'S' || test == 'E' || test == 'W')
-		game->player.posY -= game->player.planeX * game->player.m_speed;
+	{
+		if (game->player.posY > 1.1 && (see_matrix_size(game->map) - 1) > game->player.posY + 0.2 && game->player.posX > 1.2)
+		{
+			game->player.posY -= game->player.planeX * game->player.m_speed;
+		}
+		else if(game->player.planeX < 0 && game->player.posY > 1 && (see_matrix_size(game->map) - 2) > game->player.posY && game->player.posX > 1.2)
+		{
+			game->player.posY -= game->player.planeX * game->player.m_speed;
+		}
+		else if (game->player.planeX > 0 && game->player.posY > 1.1 && game->player.posX > 1.2)
+		{
+			game->player.posY -= game->player.planeX * game->player.m_speed;
+		}
+	}
 	test = game->map[(int)(game->player.posY)][(int)(game->player.posX - game->player.planeY * 0.1f)];
 	if (test == '0' || test == 'O' || test == ' ' || test == 'N' || test == 'S' || test == 'E' || test == 'W')
-		game->player.posX += game->player.planeY * game->player.m_speed;
+	{
+		printf("posY: %f\n", game->player.posY);
+		printf("posX: %f\n", game->player.posX);
+		printf("PlaneY: %f\n", game->player.planeY);
+		printf("----------------------------\n");
+		printf("\n");
+		if (game->player.posX > 1.2 && game->player.posY > 1.1 && ft_strlen(game->map[(int)game->player.posY]) - 2 > game->player.posX)
+			game->player.posX += game->player.planeY * game->player.m_speed;
+		else if (game->player.posX > 1.05 && game->player.posY > 1.1 && game->player.planeY > 0)
+			game->player.posX += game->player.planeY * game->player.m_speed;
+	}
 }
 
 void move_player(t_game *game)
